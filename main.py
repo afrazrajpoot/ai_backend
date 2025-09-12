847860a7a982244b3a405da18e7f9f5353be13c8
MuzamilEng <muzekhan777@gmail.com>
2025-09-13 04:40:11 +0500

    updated server path

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_origins=[
         "https://geniusfactor.ai",
         "https://www.geniusfactor.ai",
-        "http://api.geniusfactor.ai",
+        "https://api.geniusfactor.ai",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:8000",
